da853b38a25690c3a47bc00189072963c2026e48
xueming <810505339@qq.com>
2023-11-12 15:47:29 +0800

    添加扫描图片

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,26 +3,27 @@
     <view class="p-5">
       <view class="flex items-center justify-between">
         <text class="text-xl text-white">{{ t('home.text') }}</text>
-        
+
       </view>
       <view class="relative h-24  box mt-5 flex items-center justify-end rounded-xl">
         <view class="absolute boxicon z-10 left-0 bottom-0">
-          <image :src="boxIcon"  mode="heightFix" />
+          <image :src="boxIcon" mode="heightFix" />
         </view>
         <view class="h-full flex flex-col justify-between p-4">
           <view><text class="text-[20px] text-white">{{ t('home.text1') }}</text></view>
           <view><text class="text-[#EE2737FF] text-4xl font-bold ">5,465</text><text class="text-[20px] text-white">{{
             t('home.text2') }}</text></view>
         </view>
-        <view class="h-full w-[180px] flex justify-center items-center shadow-md  border-[#000000BF] border-l">
-          <text class="text-[20px] text-white opacity-50">{{ t('home.text3') }}</text>
+        <view class="h-full w-[180px] flex flex-col justify-center items-center shadow-md  border-[#000000BF] border-l">
+          <image :src="box1Icon" class="w-12 h-12" />
+          <text class="text-[20px] text-white opacity-50 mt-2.5">{{ t('home.text3') }}</text>
         </view>
       </view>
       <view class="mt-5">
         <text class="text-xl text-white">{{ t('home.text4') }}</text>
 
         <view v-for="item, index in list" class="h-24 p-2.5 bg-[#161616BF]  rounded-xl my-2.5" :key="index">
-          <view  class=" text-white text-sx">
+          <view class=" text-white text-sx">
             <text>门票区域/10张/预定门票</text>
           </view>
           <view class="opacity-50 text-white text-xs font-semibold">
@@ -43,7 +44,9 @@
 <script setup lang="ts">
 import baseView from '@/components/baseview/index.vue'
 import boxIcon from '@/assets/img/home/box.png'
+import box1Icon from '@/assets/img/home/box1.png'
 import { useI18n } from 'vue-i18n'
+
 const list = Array.from({ length: 10 })
 
 const { t } = useI18n()
